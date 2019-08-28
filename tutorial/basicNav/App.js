01ad74aa4d02e35e1649c1a26c9bd22cbaf339e8
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-28 11:50:43 +0100

    finished bottom tab navigation

--- tutorial/basicNav/App.js
+++ tutorial/basicNav/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet,Image} from "react-native";
 import {createAppContainer,createBottomTabNavigator, createStackNavigator, createDrawerNavigator} from "react-navigation"
 import HomeScreen from "./src/Home"
 import UserScreen from "./src/User"
 import Logo from "./src/Logo"
 import SideDrawer from "./src/SideDrawer"
+import Icon from "./src/assets/images/download.jpg"
+import Icon2 from "./src/assets/images/Screenshot_1566912554.png"
 
 
 // const appNavigation = createStackNavigator({
@@ -61,36 +63,51 @@ import SideDrawer from "./src/SideDrawer"
 //   drawerBackgroundColor:"#ffffff",
 //   drawerPosition:"right",
 // })
-const appNavigation= createBottomTabNavigator({
-  Home:HomeScreen,
-  Users:UserScreen
-},{
-  initialRouteName:"Users",
-   order:["Users", "Home"],
-   tabBarOptions:{
-    //  activeBackgroundColor:"#dbdbdb",
-    //  activeTintColor:"red",
-     showLabel:true,
-     allowFontScaling:true,
-     style:{
-       backgroundColor: "green",
-     },
-     labelStyle:{
-       fontSize: 20,
-     },
-   },
-   defaultNavigationOptions:({})=>({
-     tabBarOnPress:({navigation,defaultHandler})=>{
-       if(navigation.state.routeName==="Home"){
-         alert("Do You what to Leave")
-         navigation.navigate("Home")
-       }
-       else{
-       defaultHandler()          
-       }
-     }  
-  })
-})
+// const appNavigation= createBottomTabNavigator({
+//   Home:{
+//    screen:HomeScreen,
+//    navigationOptions:({navigate})=>({
+//      tr
+//    })
+//   },
+//   Users:UserScreen
+// },{
+//   initialRouteName:"Users",
+//    order:["Users", "Home"],
+//    tabBarOptions:{
+//     //  activeBackgroundColor:"#dbdbdb",
+//     //  activeTintColor:"red",
+//      showLabel:true,
+//      allowFontScaling:true,
+//      style:{
+//        backgroundColor: "green",
+//      },
+//      labelStyle:{
+//        fontSize: 20,
+//      },
+//    },
+//    defaultNavigationOptions:({navigation})=>({
+//      tabBarIcon:({focused, horizontal, tintColor})=>{
+//        const {routeName} = navigation.state
+//        let iconPic =null;
+//        if (routeName=="Home") {
+//           iconPic =Icon
+//        } else {
+//            iconPic = Icon2
+//        }
+//        return <Image source={iconPic} style={{width:20, height:20}}/>
+//      },
+//      tabBarOnPress:({navigation,defaultHandler})=>{
+//        if(navigation.state.routeName==="Home"){
+//          alert("Do You what to Leave")
+//          navigation.navigate("Home")
+//        }
+//        else{
+//        defaultHandler()          
+//        }
+//      }  
+//   })
+// })
 
 
 export default createAppContainer(appNavigation);
